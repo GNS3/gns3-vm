1b63891ce9706be9692ab8a6988642759318a9ba
Julien Duponchelle <julien@duponchelle.info>
2015-06-11 23:05:52 +0200

    Try to reduce VM size
    Ref #9

--- scripts/cleaner.sh
+++ scripts/cleaner.sh
@@ -1,10 +1,10 @@
-sudo apt-get purge -y vim-common fonts-ubuntu-font-family-console linux-headers-generic linux-headers-virtual vim-runtime screen tmux memtest86+ usbutils linux-firmware wpasupplicant wireless-tools wireless-regdb xauth ppp logrotate w3m python2.7-minimal man-db sgml-base mtr-tiny rsyslog lshw geoip-database groff-base fuse update-manager-core ntpdate apport bc aptitude libtext-iconv-perl libpython2.7-minimal libapparmor-perl liblocale-gettext-perl libtext-charwidth-perl unattended-upgrades ntfs-3g manpages xkb-data krb5-locales gcc-4.9 git cmake-data cpp-4.9 language-pack-en gcc gcc-4.8 rsyslog
+sudo apt-get purge -y vim-common fonts-ubuntu-font-family-console linux-headers-generic linux-headers-virtual vim-runtime screen tmux memtest86+ usbutils linux-firmware wpasupplicant wireless-tools wireless-regdb xauth ppp logrotate w3m python2.7-minimal man-db sgml-base mtr-tiny rsyslog lshw geoip-database groff-base fuse update-manager-core ntpdate apport bc aptitude libtext-iconv-perl libpython2.7-minimal libapparmor-perl liblocale-gettext-perl libtext-charwidth-perl unattended-upgrades ntfs-3g manpages xkb-data krb5-locales gcc-4.9 git cmake-data cpp-4.9 language-pack-en gcc gcc-4.8 rsyslog language-pack-gnome-en-base
 
 # Purge old kernels
 dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
 
 dpkg -l 'linux-headers-*' |grep '^ii'|sed 's/^ii[ ]*//'|cut -f1 -d' '| xargs sudo apt-get -y purge
-
+dpkg -l 'linux-image-extra-*' |grep '^ii'|sed 's/^ii[ ]*//'|cut -f1 -d' '| xargs sudo apt-get -y purge
 
 
 # Purge locale
@@ -19,5 +19,7 @@ sudo apt-get -y clean
 
 sudo rm -fr /var/lib/apt/lists/*
 sudo rm -fr /var/cache/apt/*
+sudo rm -fr /var/cache/debconf/*
+sudo rm -fr /var/cache/man/*
 sudo rm -Rf /var/log/installer/*
 sudo rm -Rf /usr/share/doc
