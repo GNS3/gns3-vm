b423abbdf8207723841ba0ed61c7197d773c8b0c
Julien Duponchelle <julien@duponchelle.info>
2015-07-02 12:17:18 +0200

    Add a second disk to the VMware VM and fix vt flag export

--- vmware.sh
+++ vmware.sh
@@ -8,4 +8,4 @@ rm -Rf output-vmware-vmx
 packer build -only=vmware-iso gns3.json
 packer build -only=vmware-vmx gns3_compress.json
 
-ovftool --overwrite output-vmware-vmx/GNS3\ VM.vmx GNS3\ VM.ova
+ovftool --extraConfig:vhv.enable=true --allowAllExtraConfig --overwrite output-vmware-vmx/GNS3\ VM.vmx GNS3\ VM.ova
