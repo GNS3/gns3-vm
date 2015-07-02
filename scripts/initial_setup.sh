70c715275500254b83355a23f362f0a345fe80d0
Julien Duponchelle <julien@duponchelle.info>
2015-07-02 16:45:14 +0200

    Mark build as testing at beginning of the boot process

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -4,13 +4,13 @@
 
 set -e
 
+echo -n "testing" > /home/gns3/.config/GNS3/gns3_release
+
 curl "https://raw.githubusercontent.com/GNS3/gns3-vm/master/scripts/welcome.py" > /tmp/gns3welcome.py
 sudo mv "/tmp/gns3welcome.py" "/usr/local/bin/gns3welcome.py"
 sudo chmod 755 "/usr/local/bin/gns3welcome.py"
 
 pip3 install --ignore-installed --pre gns3-server
-
-echo -n "testing" > /home/gns3/.config/GNS3/gns3_release
 
 echo "Reboot in 5s"
 sleep 5
