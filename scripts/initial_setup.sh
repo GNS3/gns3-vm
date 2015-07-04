21b701dbff3a0b7e18980fffda0c25e30d8f767e
Julien Duponchelle <julien@duponchelle.info>
2015-07-04 10:49:56 +0200

    Give permission on the release file

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -5,6 +5,8 @@
 set -e
 
 echo -n "testing" > /home/gns3/.config/GNS3/gns3_release
+chown gns3 /home/gns3/.config/GNS3/gns3_release
+
 
 curl "https://raw.githubusercontent.com/GNS3/gns3-vm/master/scripts/welcome.py" > /tmp/gns3welcome.py
 sudo mv "/tmp/gns3welcome.py" "/usr/local/bin/gns3welcome.py"
