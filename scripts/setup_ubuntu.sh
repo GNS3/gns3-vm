1ad08d7f97468bacaa004ec037fb4e45abd3e724
Julien Duponchelle <julien@duponchelle.info>
2015-07-05 11:12:45 +0200

    Ensure disk data drive is correctly mount
    Fix #23

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -33,6 +33,6 @@ EOF
 echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/sdb
 mkfs.ext4 /dev/sdb1
 echo "/dev/sdb1  /opt  ext4  nodiratime  0  2" >> /etc/fstab
-mount -o remount,noexec /opt
+mount -a
 
 echo "Setup ubuntu OK"
