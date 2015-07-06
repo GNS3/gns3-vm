b6d91ad4e143c627a03683a329d953f8cab0db39
Julien Duponchelle <julien@duponchelle.info>
2015-07-06 14:15:11 +0200

    Store the version number of the GNS3 VM in the VM

--- vmware.sh
+++ vmware.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-export PATH=$PATH:/Applications/VMware\ OVF\ Tool/
 set -e
+
+
+export PATH=$PATH:/Applications/VMware\ OVF\ Tool/
+
+export GNS3VM_VERSION=`cat version`
+
+echo "Build VM $GNS3VM_VERSION"
 
 rm -Rf output-vmware-iso
 rm -Rf output-vmware-vmx
 packer build -only=vmware-iso gns3.json
 packer build -only=vmware-vmx gns3_compress.json
 
-ovftool --extraConfig:vhv.enable=true --allowAllExtraConfig --overwrite output-vmware-vmx/GNS3\ VM.vmx GNS3\ VM.ova
+ovftool --extraConfig:vhv.enable=true --allowAllExtraConfig --overwrite output-vmware-vmx/GNS3\ VM.vmx GNS3\ VM\ ${GNS3VM_VERSION}.ova
