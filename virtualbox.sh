e28a71d10f50d0822ae638ce70e84af0a5c66f33
Julien Duponchelle <julien@duponchelle.info>
2015-07-06 17:50:21 +0200

    Virtual Box build

--- virtualbox.sh
+++ virtualbox.sh
@@ -2,5 +2,9 @@
 
 set -e
 
+export GNS3VM_VERSION=`cat version`
+
+echo "Build VM $GNS3VM_VERSION"
+
 packer build -only=virtualbox-iso gns3.json
 packer build -only=virtualbox-ovf gns3_compress.json
