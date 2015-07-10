2ad3e9850bbe8fa84353acfb27aa87e71bd1618c
Julien Duponchelle <julien@duponchelle.info>
2015-07-10 13:56:45 +0200

    Fix virtual box renaming

--- virtualbox.sh
+++ virtualbox.sh
@@ -7,4 +7,4 @@ export GNS3VM_VERSION=`cat version`
 echo "Build VM $GNS3VM_VERSION"
 
 packer build -force -only=virtualbox-iso gns3.json
-mv output-virtualbox-iso/GSN3\ VM.ova GNS3\ VM\ VirtualBox\ ${GNS3VM_VERSION}.ova
+mv output-virtualbox-iso/GNS3\ VM.ova GNS3\ VM\ VirtualBox\ ${GNS3VM_VERSION}.ova
