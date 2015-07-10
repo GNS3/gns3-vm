d2846746970ec388bdd3963bf8e3bbcc57a9e2ad
Julien Duponchelle <julien@duponchelle.info>
2015-07-10 14:11:57 +0200

    Install update for welcome script from the unstable branch also

--- scripts/update_unstable.sh
+++ scripts/update_unstable.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
 # Update script called from the GNS 3 VM
-# 
+#
 
 set -e
 
-curl "https://raw.githubusercontent.com/GNS3/gns3-vm/master/scripts/welcome.py" > /tmp/gns3welcome.py
+curl "https://raw.githubusercontent.com/GNS3/gns3-vm/unstable/scripts/welcome.py" > /tmp/gns3welcome.py
 sudo mv "/tmp/gns3welcome.py" "/usr/local/bin/gns3welcome.py"
 sudo chmod 755 "/usr/local/bin/gns3welcome.py"
 
 cd ~
 if [ ! -d "gns3-server" ]
-then 
+then
     sudo apt-get install -y git
     git clone https://github.com/GNS3/gns3-server.git gns3-server
 fi
