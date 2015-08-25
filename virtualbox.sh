27a8376a5dff4bb58332beac576e49deb8fb5662
Julien Duponchelle <julien@duponchelle.info>
2015-08-25 21:52:13 +0200

    Allow to pass parameters to VirtualBox build

--- virtualbox.sh
+++ virtualbox.sh
@@ -6,7 +6,7 @@ export GNS3VM_VERSION=`cat version`
 
 echo "Build VM $GNS3VM_VERSION"
 
-packer build -force -only=virtualbox-iso gns3.json
+packer build -force -only=virtualbox-iso $* gns3.json
 
 cd output-virtualbox-iso
 zip -9 "../GNS3 VM VirtualBox ${GNS3VM_VERSION}.zip" "GNS3 VM.ova"
