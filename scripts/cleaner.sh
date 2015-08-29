bf0e9e4970842e4964a2207c8db73b40e57f1598
Julien Duponchelle <julien@duponchelle.info>
2015-08-29 19:03:40 +0200

    Remove package one by one to avoid one error breaking everything

--- scripts/cleaner.sh
+++ scripts/cleaner.sh
@@ -1,4 +1,53 @@
-sudo apt-get purge -y vim-common fonts-ubuntu-font-family-console linux-headers-generic linux-headers-virtual vim-runtime screen tmux memtest86+ usbutils linux-firmware wpasupplicant wireless-tools wireless-regdb xauth ppp logrotate w3m python2.7-minimal man-db sgml-base mtr-tiny rsyslog lshw geoip-database groff-base fuse update-manager-core ntpdate apport bc aptitude libtext-iconv-perl libpython2.7-minimal libapparmor-perl liblocale-gettext-perl libtext-charwidth-perl unattended-upgrades ntfs-3g manpages xkb-data krb5-locales gcc-4.9 git cmake-data cpp-4.9 language-pack-en gcc gcc-4.8 rsyslog language-pack-gnome-en-base
+sudo apt-get purge -y vim-common
+sudo apt-get purge -y fonts-ubuntu-font-family-console
+sudo apt-get purge -y linux-headers-generic
+sudo apt-get purge -y linux-headers-virtual
+sudo apt-get purge -y vim-runtime
+sudo apt-get purge -y screen
+sudo apt-get purge -y tmux
+sudo apt-get purge -y memtest86+
+sudo apt-get purge -y usbutils
+sudo apt-get purge -y linux-firmware
+sudo apt-get purge -y wpasupplicant
+sudo apt-get purge -y wireless-tools
+sudo apt-get purge -y wireless-regdb
+sudo apt-get purge -y xauth
+sudo apt-get purge -y ppp
+sudo apt-get purge -y logrotate
+sudo apt-get purge -y w3m
+sudo apt-get purge -y python2.7-minimal
+sudo apt-get purge -y man-db
+sudo apt-get purge -y sgml-base
+sudo apt-get purge -y mtr-tiny
+sudo apt-get purge -y rsyslog
+sudo apt-get purge -y lshw
+sudo apt-get purge -y geoip-database
+sudo apt-get purge -y groff-base
+sudo apt-get purge -y fuse
+sudo apt-get purge -y update-manager-core
+sudo apt-get purge -y ntpdate
+sudo apt-get purge -y apport
+sudo apt-get purge -y bc
+sudo apt-get purge -y aptitude
+sudo apt-get purge -y libtext-iconv-perl
+sudo apt-get purge -y libpython2.7-minimal
+sudo apt-get purge -y libapparmor-perl
+sudo apt-get purge -y liblocale-gettext-perl
+sudo apt-get purge -y libtext-charwidth-perl
+sudo apt-get purge -y unattended-upgrades
+sudo apt-get purge -y ntfs-3g
+sudo apt-get purge -y manpages
+sudo apt-get purge -y xkb-data
+sudo apt-get purge -y krb5-locales
+sudo apt-get purge -y gcc-4.9
+sudo apt-get purge -y git
+sudo apt-get purge -y cmake-data
+sudo apt-get purge -y cpp-4.9
+sudo apt-get purge -y language-pack-en
+sudo apt-get purge -y gcc
+sudo apt-get purge -y gcc-4.8
+sudo apt-get purge -y rsyslog
+sudo apt-get purge -y language-pack-gnome-en-base
 
 # Purge old kernels
 dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
