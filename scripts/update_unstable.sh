edccc51922049762021930147ca088f6762abde1
Julien Duponchelle <julien@duponchelle.info>
2015-12-21 15:45:39 +0100

    Use 1.4dev when coming from unstable

--- scripts/update_unstable.sh
+++ scripts/update_unstable.sh
@@ -1,3 +1,3 @@
 # For compatibility with previous GNS3 VM version
-curl "https://raw.githubusercontent.com/GNS3/gns3-vm/unstable/scripts/update_1.4.sh" | bash
+curl "https://raw.githubusercontent.com/GNS3/gns3-vm/unstable/scripts/update_1.4dev.sh" | bash
 
