b4f2706e0127d90694282bb3b346a7fcdbeeb4e5
Julien Duponchelle <julien@duponchelle.info>
2016-05-13 11:18:18 +0200

    Hardcode home directory

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -40,43 +40,40 @@ sudo mv "/tmp/gns3welcome.py" "/usr/local/bin/gns3welcome.py"
 sudo chmod 755 "/usr/local/bin/gns3welcome.py"
 
 
-
-cd ~
-
 # The upgrade from 0.8 to 0.8.6 is safe
-if [ `cat .config/GNS3/gns3vm_version` = '0.8' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.8.1' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.8.2' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.8.3' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.8.4' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.8.5' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.0' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.1' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.2' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.3' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.4' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.5' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.6' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.7' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.8' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.9' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.9.10' ]
+if [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.8' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.8.1' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.8.2' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.8.3' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.8.4' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.8.5' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.0' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.1' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.2' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.3' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.4' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.5' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.6' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.7' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.8' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.9' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.9.10' ]
 then
     sudo apt-get -y dist-upgrade
     sudo usermod -a -G vde2-net gns3
     
-    echo -n '0.10.0' > .config/GNS3/gns3vm_version
+    echo -n '0.10.0' > /home/gns3/.config/GNS3/gns3vm_version
 fi
 
-if [ `cat .config/GNS3/gns3vm_version` = '0.10.0' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.10.1' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.10.2' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.10.3' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.10.4' ] \
-    || [ `cat .config/GNS3/gns3vm_version` = '0.10.5' ]
+if [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.10.0' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.10.1' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.10.2' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.10.3' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.10.4' ] \
+    || [ `cat /home/gns3/.config/GNS3/gns3vm_version` = '0.10.5' ]
 then
     sudo rm -f /usr/local/bin/vpcs
-    sed '/port = 8000$/d' -i ~/.config/GNS3/gns3_server.conf
-    echo -n '0.10.6' > .config/GNS3/gns3vm_version
+    sed '/port = 8000$/d' -i /home/gns3/.config/GNS3/gns3_server.conf
+    echo -n '0.10.6' > /home/gns3/.config/GNS3/gns3vm_version
 fi
 
