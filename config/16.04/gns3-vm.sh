eb9f212a182b82e0976173396b80dbf83362c484
Julien Duponchelle <julien@gns3.net>
2017-04-12 11:24:34 +0200

    Fix no network on eth1 and detection issues

--- config/16.04/gns3-vm.sh
+++ config/16.04/gns3-vm.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 command -v vmtoolsd >/dev/null 2>&1 || exit 0
 
-IP=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1`
+IP=""
+while [ "$IP" == "" ]
+do
+    IP=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1`
+    sleep 1
+done
 
 vmtoolsd --cmd "info-set guestinfo.gns3.eth0 $IP"
-
+exit 0
 
