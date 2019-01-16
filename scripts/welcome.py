dc12d80eddc1a91037a3061432212bda7638e1d9
grossmj <grossmj@gns3.net>
2019-01-16 16:34:30 +0700

    Fix issue with double quotes for migrate.

--- scripts/welcome.py
+++ scripts/welcome.py
@@ -277,7 +277,7 @@ def migrate():
             script = """
 if [ ! -f ~/.ssh/gns3-vm-key ]
 then
-    ssh-keygen -f ~/.ssh/gns3-vm-key -N ""
+    ssh-keygen -f ~/.ssh/gns3-vm-key -N ''
 fi
 ssh-copy-id -i ~/.ssh/gns3-vm-key gns3@{}
 """.format(destination)
