1a16f4b743061d6f6ed07880ad64d751cefeb539
grossmj <grossmj@gns3.net>
2019-03-11 22:08:12 +0700

    Activate nested virtualization for GNS3 VM on Hyper-V

--- create-vm.ps1
+++ create-vm.ps1
@@ -1,3 +1,4 @@
 New-VM -Name "GNS3 VM" -Generation 1 -MemoryStartupBytes 1GB -SwitchName "Default Switch"
 Add-VMHardDiskDrive -VMName "GNS3 VM" -Path "GNS3_VM-disk1.vhd"
 Add-VMHardDiskDrive -VMName "GNS3 VM" -Path "GNS3_VM-disk2.vhd"
+Set-VMProcessor -VMName "GNS3 VM" -ExposeVirtualizationExtensions $true
