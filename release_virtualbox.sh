42a1af2b085b87b5352323d5ff773709e11ae039
grossmj <grossmj@gns3.net>
2019-03-14 17:49:04 +0700

    Force using v0.10.15 for base GNS3 VM.

--- release_virtualbox.sh
+++ release_virtualbox.sh
@@ -28,8 +28,8 @@ export GNS3_SRC="/tmp/GNS3VM.VirtualBox.${GNS3VM_VERSION}.ova"
 
 if [[ "$GNS3_VM_FILE" == "" ]]
 then
-    export GNS3VM_VERSION=`python last_vm_version.py`
-    export GNS3VM_URL="https://github.com/GNS3/gns3-vm/releases/download/v${GNS3VM_VERSION}/GNS3VM.VMware.${GNS3VM_VERSION}.zip"
+    export GNS3VM_VERSION="0.10.15" # `python last_vm_version.py`
+    export GNS3VM_URL="https://github.com/GNS3/gns3-vm/releases/download/v${GNS3VM_VERSION}/GNS3VM.VirtualBox.${GNS3VM_VERSION}.zip"
     echo "Download the base GNS3 VM version ${GNS3VM_VERSION} from GitHub"
     if [[ ! -f "/tmp/GNS3VM.VirtualBox.${GNS3VM_VERSION}.zip" ]]
     then
