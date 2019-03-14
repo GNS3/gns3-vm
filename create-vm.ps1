94918ad372a7c8ef8b49cb795533f0f5511fb19c
grossmj <grossmj@gns3.net>
2019-03-14 15:33:18 +0700

    Enable MacAddressSpoofing for GNS3 VM on Hyper-V

--- create-vm.ps1
+++ create-vm.ps1
@@ -2,3 +2,4 @@ New-VM -Name "GNS3 VM" -Generation 1 -MemoryStartupBytes 1GB -SwitchName "Defaul
 Add-VMHardDiskDrive -VMName "GNS3 VM" -Path "GNS3_VM-disk1.vhd"
 Add-VMHardDiskDrive -VMName "GNS3 VM" -Path "GNS3_VM-disk2.vhd"
 Set-VMProcessor -VMName "GNS3 VM" -ExposeVirtualizationExtensions $true
+Set-VMNetworkAdapter -VMName "GNS3 VM" -MacAddressSpoofing On
