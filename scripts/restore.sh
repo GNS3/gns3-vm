a25c4d58a31bfd3283392b8456b1bb18df291116
grossmj <grossmj@gns3.net>
2019-03-15 23:03:37 +0700

    Change restore.sh

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -2,4 +2,4 @@
 
 # Used by the GNS3 restore command
 export BRANCH="bionic-stable"
-curl "https://raw.githubusercontent.com/GNS3/gns3-vm/$BRANCH/scripts/update_2.1.sh" | bash
+curl "https://raw.githubusercontent.com/GNS3/gns3-vm/$BRANCH/scripts/upgrade_2.2dev.sh" | bash
