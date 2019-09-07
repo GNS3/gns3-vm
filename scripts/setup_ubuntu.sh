34cd3dc9b2890a5996da811982df34de3421ee7b
grossmj <grossmj@gns3.net>
2019-09-07 15:51:22 +0700

    Use UUID for /dev/sdb1 in /etc/fstab. Ref https://github.com/GNS3/gns3-gui/issues/2818

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -13,7 +13,7 @@ apt-get update
 # Create the /opt disk
 echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/sdb
 mkfs.ext4 /dev/sdb1
-echo "/dev/sdb1  /opt  ext4  nodiratime  0  2" >> /etc/fstab
+echo "UUID=$(blkid -s UUID -o value /dev/sdb1)  /opt  ext4  nodiratime  0  2" >> /etc/fstab
 mount -a
 
-echo "Ubuntu has been "
+echo "Ubuntu has been setup"
