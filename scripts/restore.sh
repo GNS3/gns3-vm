a0c5eeeb7075289c73c05e30e0b9ed9ec44e39db
grossmj <grossmj@gns3.net>
2019-10-04 14:18:59 +0800

    Use correct branches for restore and qemu scripts

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Used by the GNS3 restore command
-export BRANCH="bionic-unstable"
+export BRANCH="bionic-stable"
 curl -Lk "https://raw.githubusercontent.com/GNS3/gns3-vm/$BRANCH/scripts/upgrade_2.2dev.sh" | bash
