ab92f68a5a7b9835334aa170bcd09430ab782286
grossmj <grossmj@gns3.net>
2020-03-27 20:03:52 +1030

    Fix KVM VM build

--- release_kvm.sh
+++ release_kvm.sh
@@ -23,13 +23,13 @@ echo "Build VM for GNS3 $GNS3_VERSION"
 echo "Release channel: $GNS3_RELEASE_CHANNEL"
 
 
-if [[ ! -f "./GNS3 VM VirtualBox ${GNS3_VERSION}.zip" ]]
+if [[ ! -f "./GNS3.VM.VirtualBox.${GNS3_VERSION}.zip" ]]
 then
     export GNS3VM_URL="https://github.com/GNS3/gns3-gui/releases/download/v${GNS3_VERSION}/GNS3.VM.VirtualBox.${GNS3_VERSION}.zip"
     echo "Download the base GNS3 VM version ${GNS3VM_VERSION} from GitHub"
     curl --insecure -L "$GNS3VM_URL" > "/tmp/GNS3VM.VirtualBox.${GNS3_VERSION}.zip"
 else
-    cp "./GNS3 VM VirtualBox ${GNS3_VERSION}.zip" "/tmp/GNS3VM.VirtualBox.${GNS3_VERSION}.zip"
+    cp "./GNS3.VM.VirtualBox.${GNS3_VERSION}.zip" "/tmp/GNS3VM.VirtualBox.${GNS3_VERSION}.zip"
 fi
 
 unzip -p "/tmp/GNS3VM.VirtualBox.${GNS3_VERSION}.zip" "GNS3 VM.ova" > "/tmp/GNS3VM.VirtualBox.${GNS3_VERSION}.ova"
