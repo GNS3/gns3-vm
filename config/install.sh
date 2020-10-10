de0767e4bc8d1c5d56b8ddc04354f863d9b8b5a6
grossmj <grossmj@gns3.net>
2020-10-10 11:26:11 +1030

    Revert upgrade to version 19.03.13

--- config/install.sh
+++ config/install.sh
@@ -95,13 +95,13 @@ fi
 # Install o/qther GNS3 dependencies
 apt-get install -y gns3-iou dynamips vpcs ubridge
 
-# Install Docker if not installed or version is not 19.03.13
-if [[ ! $(which docker) ]] || [[ ! $(docker --version | grep "19.03.13") ]]; then
-   sudo apt-get purge -y docker-ce
-   curl -sSLk https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_19.03.13~3-0~ubuntu-bionic_amd64.deb > /tmp/docker.deb
+# Install Docker if not installed or version is not 18.06.1
+if [[ ! $(which docker) ]] || [[ ! $(docker --version | grep "18.06.1") ]]; then
+   curl -sSLk https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb > /tmp/docker.deb
    sudo apt-get install -y libltdl7
    sudo dpkg -i /tmp/docker.deb
 fi
+
 sudo usermod -aG docker gns3
 sudo service docker stop
 sudo rm -rf /var/lib/docker/aufs
