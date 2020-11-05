0bdc8caa5e398b5cfdec3aa7dbecc3b7f4755dda
grossmj <grossmj@gns3.net>
2020-11-05 14:44:42 +1030

    Fix restore script for stable VM.

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -2,4 +2,4 @@
 
 # Used by the GNS3 restore command
 export BRANCH="bionic-stable"
-curl -Lk "https://raw.githubusercontent.com/GNS3/gns3-vm/$BRANCH/scripts/upgrade_2.2dev.sh" | bash
+curl -Lk "https://raw.githubusercontent.com/GNS3/gns3-vm/$BRANCH/scripts/upgrade_2.2.sh" | bash
