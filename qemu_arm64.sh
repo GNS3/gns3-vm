a69dfe7d442da92a7cf1c259ef0451d408ace16d
grossmj <grossmj@gns3.net>
2021-11-21 20:33:03 +1030

    Fix ARM build and bump base VM version to 0.12.1

--- qemu_arm64.sh
+++ qemu_arm64.sh
@@ -14,28 +14,27 @@ sudo mount -r ubuntu-20.04.3-live-server-arm64.iso ubuntu-arm64-iso
 
 sudo apt update && sudo apt install -y qemu-utils qemu-system-arm qemu-efi-aarch64
 
-rm gns3vm-disk*
 qemu-img create -f qcow2 gns3vm-disk1.qcow2 20G
 qemu-img create -f qcow2 gns3vm-disk2.qcow2 500G
 
 nohup python3 -m http.server --directory http 4242 &
+
+echo "Build VM $GNS3VM_VERSION for ARM64"
 
 qemu-system-aarch64 -name "GNS3 VM" -nographic -m 2048 -machine virt -cpu max -smp 4 \
 -netdev user,id=vnet,hostfwd=:127.0.0.1:0-:22 -device virtio-net-pci,netdev=vnet \
 -drive file=gns3vm-disk1.qcow2,if=virtio,cache=none,format=qcow2 \
 -drive file=gns3vm-disk2.qcow2,if=virtio,cache=none,format=qcow2 \
 -bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd \
--kernel iso/casper/vmlinuz -initrd iso/casper/initrd \
+-kernel ubuntu-arm64-iso/casper/vmlinuz -initrd ubuntu-arm64-iso/casper/initrd \
 -append "autoinstall ds=nocloud-net;s=http://10.0.2.2:4242/ console=ttyAMA0" \
 -cdrom ubuntu-20.04.3-live-server-arm64.iso -no-reboot -boot strict=off
 
-echo "Build VM $GNS3VM_VERSION for ARM64"
-
 packer build -only=qemu $* gns3.json
+
+rm -Rf output-qemu
 
 cp gns3vm-disk1.qcow2 gns3vm-disk1.qcow2.bak
 qemu-img convert -O qcow2 gns3vm-disk1.qcow2.bak gns3vm-disk1.qcow2
 
-zip -9 "../GNS3VM.ARM64.${GNS3VM_VERSION}.zip" gns3vm-disk1.qcow2 gns3vm-disk2.qcow2
-
-rm -Rf output-*
+zip -9 "GNS3VM.ARM64.${GNS3VM_VERSION}.zip" gns3vm-disk1.qcow2 gns3vm-disk2.qcow2
--- release_qemu_arm64.sh
+++ release_qemu_arm64.sh
@@ -8,6 +8,7 @@
 set -e
 
 export GNS3_VERSION=`echo $1 | sed "s/^v//"`
+export GNS3_VM_FILE=$2
 
 if [[ "$GNS3_VERSION" == "" ]]
 then
@@ -22,21 +23,25 @@ export GNS3_RELEASE_CHANNEL="2.2"
 echo "Build VM for GNS3 $GNS3_VERSION"
 echo "Release channel: $GNS3_RELEASE_CHANNEL"
 
-
-if [[ ! -f "./GNS3.VM.ARM64.${GNS3_VERSION}.zip" ]]
+if [[ "$GNS3_VM_FILE" == "" ]]
 then
-    export GNS3VM_URL="https://github.com/GNS3/gns3-gui/releases/download/v${GNS3_VERSION}/GNS3.VM.ARM64.${GNS3_VERSION}.zip"
+    export GNS3VM_VERSION="0.12.1" # `python last_vm_version.py`
+    export GNS3VM_URL="https://github.com/GNS3/gns3-vm/releases/download/v${GNS3VM_VERSION}/GNS3.VM.ARM64.${GNS3VM_VERSION}.zip"
     echo "Download the base GNS3 VM version ${GNS3VM_VERSION} from GitHub"
     curl --insecure -L "$GNS3VM_URL" > "/tmp/GNS3VM.ARM64.${GNS3_VERSION}.zip"
 else
-    cp "./GNS3.VM.ARM64.${GNS3_VERSION}.zip" "/tmp/GNS3VM.ARM64.${GNS3_VERSION}.zip"
+    echo "GNS3 VM file: $GNS3_VM_FILE"
+    cp "$GNS3_VM_FILE" "/tmp/GNS3VM.ARM64.${GNS3VM_VERSION}.zip"
 fi
 
 unzip "/tmp/GNS3VM.ARM64.${GNS3_VERSION}.zip"
 
 packer build -only=qemu gns3_release.json
 
-for qcow2_file in *.qcow2; do
+rm -Rf output-qemu
+
+for qcow2_file in *.qcow2
+do
     echo "Converting ${qcow2_file} to VMDK format..."
     vmdk_file=`basename "${qcow2_file}" .qcow2`
     qemu-img convert -O vmdk "${qcow2_file}" "${vmdk_file}.vmdk"
