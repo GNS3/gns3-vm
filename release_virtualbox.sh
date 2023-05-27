4c7e517e62fd577fe7939e331f970a4529e41233
grossmj <grossmj@gns3.net>
2023-05-27 15:57:40 +0800

    Build base VM 0.15.0

--- release_virtualbox.sh
+++ release_virtualbox.sh
@@ -27,7 +27,7 @@ export GNS3_SRC="/tmp/GNS3VM.VirtualBox.${GNS3VM_VERSION}.ova"
 
 if [[ "$GNS3_VM_FILE" == "" ]]
 then
-    export GNS3VM_VERSION="0.14.0" # `python last_vm_version.py`
+    export GNS3VM_VERSION="0.15.0" # `python last_vm_version.py`
     export GNS3VM_URL="https://github.com/GNS3/gns3-vm/releases/download/v${GNS3VM_VERSION}/GNS3VM.VirtualBox.${GNS3VM_VERSION}.zip"
     echo "Download the base GNS3 VM version ${GNS3VM_VERSION} from GitHub"
     if [[ ! -f "/tmp/GNS3VM.VirtualBox.${GNS3VM_VERSION}.zip" ]]
