f2d9ad49cff8843ad679bac7d65d7dc99fe48233
grossmj <grossmj@gns3.net>
2023-06-06 10:44:42 +0930

    Change facility to metro for Equinix metal API

--- .circleci/device.py
+++ .circleci/device.py
@@ -56,7 +56,7 @@ def get():
             project_id=gns3_project.id,
             hostname=GNS3_HOSTNAME,
             plan=server_type,
-            facility='any',
+            metro='any',
             operating_system="ubuntu_22_04")
 
     # wait max 20 min for being active
