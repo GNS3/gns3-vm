6c2a99701fa96b735b53796c7e573e48e92be5c4
grossmj <grossmj@gns3.net>
2023-06-23 16:02:26 +0930

    Avoid gpg to ask for overwriting docker key

--- config/install.sh
+++ config/install.sh
@@ -73,12 +73,15 @@ else
 fi
 
 # Set up the Docker repository
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
+
+# Fix upgrade error "ModuleNotFoundError: No module named 'debian'"
+apt install --reinstall python3-debian
 
 # Python
 apt install -y python3-dev python3-setuptools
