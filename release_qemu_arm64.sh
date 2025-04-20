0c6bc2f4c2eb0665dc07bd204c2d2eda047480c6
grossmj <grossmj@gns3.net>
2025-04-20 16:28:21 +0700

    Switch to headless and use correct 7z binary

--- release_qemu_arm64.sh
+++ release_qemu_arm64.sh
@@ -34,7 +34,7 @@ else
     cp "$GNS3_VM_FILE" "/tmp/GNS3VM.ARM64.${GNS3VM_VERSION}.zip"
 fi
 
-7zz e -y "/tmp/GNS3VM.ARM64.${GNS3VM_VERSION}.zip"
+7z e -y "/tmp/GNS3VM.ARM64.${GNS3VM_VERSION}.zip"
 
 packer build -only=qemu gns3_release.json
 
@@ -48,6 +48,6 @@ do
 done
 
 echo "Compressing VMDK files to GNS3.VM.ARM64.${GNS3_VERSION}.zip..."
-7zz a -bsp1 -mx=1 "GNS3.VM.ARM64.${GNS3_VERSION}.zip" *.vmdk
+7z a -bsp1 -mx=1 "GNS3.VM.ARM64.${GNS3_VERSION}.zip" *.vmdk
 
 exit 0
