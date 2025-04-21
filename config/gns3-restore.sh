344ad811365b7dedcba9182ed2a75cf36532199f
grossmj <grossmj@gns3.net>
2025-04-21 19:41:01 +0700

    Fix after merge

--- config/gns3-restore.sh
+++ config/gns3-restore.sh
@@ -9,5 +9,5 @@ then
     exit 1
 fi
 
-export BRANCH="focal-stable"
+export BRANCH="focal-unstable"
 curl -Lk "https://raw.githubusercontent.com/GNS3/gns3-vm/$BRANCH/scripts/restore.sh" | bash
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Used by the GNS3 restore command
-export BRANCH="focal-stable"
+export BRANCH="focal-unstable"
 curl -Lk "https://raw.githubusercontent.com/GNS3/gns3-vm/$BRANCH/scripts/upgrade_2.2.sh" | bash
