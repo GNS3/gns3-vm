52a4cfca63d101fbd0e6983413e41f92915d38db
grossmj <grossmj@gns3.net>
2026-07-15 09:13:54 +0200

    Revert "Update setup_data_disk.sh to use a disk label for the secondary disk"
    This reverts commit c70280717c4177f4b99042f3eb572b086e85a2a7.

--- scripts/setup_data_disk.sh
+++ scripts/setup_data_disk.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-fdisk -l
-
 if [[ -b "/dev/vdb" ]]
 then
   DATA_DISK="/dev/vdb"
@@ -16,8 +14,7 @@ fi
 # Create the /opt disk
 echo -e "o\nn\np\n1\n\n\nw" | fdisk $DATA_DISK
 mkfs.ext4 $DATA_PART
-e2label $DATA_PART GNS3-DATA-DISK
-echo "LABEL=GNS3-DATA-DISK  /opt  ext4  nodiratime  0  2" >> /etc/fstab
+echo "UUID=$(blkid -s UUID -o value $DATA_PART)  /opt  ext4  nodiratime  0  2" >> /etc/fstab
 mount -a
-cat /etc/fstab
+
 echo "Data disk has been setup and partition mounted on /opt"
