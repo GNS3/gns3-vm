4478c84e7e31a9495236904554425a756d60b4db
grossmj <grossmj@gns3.net>
2026-07-30 12:26:35 +0200

    Set release channel in upgrade scripts

--- scripts/upgrade_2.2.sh
+++ scripts/upgrade_2.2.sh
@@ -20,6 +20,7 @@
 set -e
 
 export BRANCH="resolute-stable"
+export RELEASE_CHANNEL="2.2"
 export UNSTABLE_APT="0"
 export PYPI_GNS3SERVER_JSON_URL="https://pypi.org/pypi/gns3-server/json"
 
--- scripts/upgrade_3.1dev.sh
+++ scripts/upgrade_3.1dev.sh
@@ -20,6 +20,7 @@
 set -e
 
 export BRANCH="resolute-unstable"
+export RELEASE_CHANNEL="3.1dev"
 export UNSTABLE_APT="1"
 
 # upgrade the GNS3 VM first
